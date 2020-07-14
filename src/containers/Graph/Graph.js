3a144f50fdff97c3e978f7830c81eabb21256678
PartyPancakess <bilgehalilonayci@gmail.com>
2020-07-14 11:31:55 +0300

    work on cytoscape

--- src/containers/Graph/Graph.js
+++ src/containers/Graph/Graph.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import CytoscapeComponent from 'react-cytoscapejs';
 
@@ -9,10 +9,22 @@ const Graph = (props) => {
     const elements = [
         { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
         { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-        { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
+        { data: { id: 'three', label: 'Node 3' }, position: { x: 200, y: 0 } },
+        { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } },
+        { data: { source: 'two', target: 'three', label: 'Edge from Node2 to Node3' } }
      ];
 
-     return <CytoscapeComponent elements={elements} style={ { width: '100%', height: '100%' } } />;
+     useEffect(() => {
+        Graph.cy.on('click', 'node', (event) => {
+            console.log(event.target._private.data);
+            var j = Graph.cy.$('#' + event.target._private.data.id);
+            Graph.cy.remove( j );
+          })
+      });
+      
+
+
+     return <CytoscapeComponent cy={(cy) => {Graph.cy = cy}} elements={elements} style={ { width: '100%', height: '100%' } } />;
 }
 
 
