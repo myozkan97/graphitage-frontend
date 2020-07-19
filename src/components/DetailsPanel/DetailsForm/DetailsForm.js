5e6d5e7af6714b770e9bca1dad2f6d93e258bde7
M. Yusuf Ozkan <myozkan@yandex.com>
2020-07-19 16:19:21 +0300

    implemented details panel

--- src/components/DetailsPanel/DetailsForm/DetailsForm.js
+++ src/components/DetailsPanel/DetailsForm/DetailsForm.js
@@ -4,18 +4,111 @@ import { connect } from 'react-redux';
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 
+
+const stringFromArray = (array, sep) => {
+    return array.reduce((acc, cur, index) => {
+        return acc + cur + sep;
+    }, "")
+}
+
+const pFromArray = (array, htmlEl = "p") => {
+    if (htmlEl === "p") {
+        return array.reduce((acc, cur, index) => {
+            acc.push((<p>{cur}</p>))
+            return acc;
+        }, [])
+    } else if (htmlEl === "list") {
+        const listArr = array.reduce((acc, cur, index) => {
+            acc.push((<li>{cur}</li>))
+            return acc;
+        }, []);
+
+        return (<ul>{listArr}</ul>)
+    }
+    
+}
+
+
 const Details = (props) => {
     return (
-        <Form>
-            <h3>
-               <br/> Details
-            </h3>
-            <p>
-               <br/> NodeId: {props.nodeId}
-               <br/> Paper Id Type: {props.dtl.paperIdType}
-               <br/> Title: {props.dtl.title}
-            </p>
-        </Form>
+        
+        <div className="Details">
+            <h2>{props.dtl.title}</h2>
+            <div className="body">
+                {props.dtl.abstractOfPaper &&
+                    <div className="abstract">
+                        <h3>Abstract</h3>
+                        <p>{props.dtl.abstractOfPaper}</p>
+                    </div>
+                }
+                {props.dtl.year &&
+                    <div className="year">
+                        <p>Year: {props.dtl.year}</p>
+                    </div>
+                }
+                {props.dtl.applicationDomains &&
+                    <div className="applicationDomains">
+                        <h3>Application Domains</h3>
+                        <p>{stringFromArray(props.dtl.applicationDomains, ", ")}</p>
+                    </div>
+                }
+                {props.dtl.components &&
+                    <div className="components">
+                        <h3>Components</h3>
+                        <p>{stringFromArray(props.dtl.components, ", ")}</p>
+                    </div>
+                }
+                {props.dtl.highlights &&
+                    <div className="highlights">
+                        <h3>Highlights</h3>
+                        <p>{pFromArray(props.dtl.highlights)}</p>
+                    </div>
+                }
+                {props.dtl.contributions &&
+                    <div className="contributions">
+                        <h3>Contributions</h3>
+                        <p>{pFromArray(props.dtl.contributions)}</p>
+                    </div>
+                }
+                {props.dtl.pros &&
+                    <div className="pros">
+                        <h3>Pros</h3>
+                        <p>{pFromArray(props.dtl.pros, "list")}</p>
+                    </div>
+                }
+                {props.dtl.notes &&
+                    <div className="notes">
+                        <h3>Notes</h3>
+                        <p>{pFromArray(props.dtl.notes)}</p>
+                    </div>
+                }
+                {props.dtl.data &&
+                    <div className="data">
+                        <h3>Datasets</h3>
+                        <p>{stringFromArray(props.dtl.data.map(obj => obj.datasetName))}</p>
+                    </div>
+                }
+                {props.dtl.libraries &&
+                    <div className="libraries">
+                        <h3>Libraries</h3>
+                        <p><ul>{props.dtl.libraries.map(obj => (<li><a href={obj.link}>{obj.name}</a></li>))}</ul></p>
+                    </div>
+                }
+                {props.dtl.relatedWorks &&
+                    <div className="relatedWorks">
+                        <h3>Related Works</h3>
+                        <p><ul>{props.dtl.relatedWorks.map(obj => (<li>{obj.title}</li>))}</ul></p>
+                    </div>
+                }
+                {props.dtl.reader &&
+                    <div className="reader">
+                        <h3>Readers</h3>
+                        <p><ul>{props.dtl.reader.map(obj => (<li>obj.readerName</li>))}</ul></p>
+                    </div>
+                }
+            </div>
+            {console.log(props.dtl.title)}
+        </div>
     )
 }
 
--- src/store/actions/details.js
+++ src/store/actions/details.js
@@ -13,28 +13,11 @@ const saveDetails = (res) => {
 
 export const fetchDetails = (id = 0) => {
     return (dispatch, getState) => {
-        // async get proccess
-
-        // fetch(url, {
-        //     method: "GET",
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'secret-key': '$2b$10$IS21t82Y4ZQ6qX6QPAKm6e5gfdSkfZJFLeX9E5XWKLKnXpZinECQC'
-        //     }
-        // })
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         dispatch(saveDetails(data));
-        //         console.log(data);
-        //     })
-
         httpReq(baseUrl + 'papers/' + id, 'GET')
         .then((result) => {
                 console.log(result.data);
                 dispatch(saveDetails(result.data));
             });
-
     }
-
 }
 
