45a5426f79c6c589821fca8f4e564e421b9b0145
M. Yusuf Ozkan <myozkan@yandex.com>
2020-07-23 14:37:36 +0300

    more refactoring

--- src/components/OptionsMenu/OptionsForm/OptionsForm.js
+++ src/components/OptionsMenu/OptionsForm/OptionsForm.js
@@ -1,93 +1,92 @@
-import React, { useEffect, useState } from "react"
+import React, { useState } from "react";
 
-import Form from "react-bootstrap/Form"
-import Button from "react-bootstrap/Button"
-import { useForm } from "react-hook-form"
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useForm } from "react-hook-form";
 
-import { connect } from "react-redux"
+import { connect } from "react-redux";
 
-import * as actionCreators from "../../../store/actions/index"
-import httpReq from "../../../store/actions/utils/http"
+import * as actionCreators from "../../../store/actions/index";
+import httpReq from "../../../store/actions/utils/http";
 
-const baseUrl = "https://graphitage.herokuapp.com/api.graphitage.com/"
-
+const baseUrl = "https://graphitage.herokuapp.com/api.graphitage.com/";
 
 const OptionsForm = (props) => {
-    const { register, handleSubmit, errors, watch, formState } = useForm({
-        mode: "onChange"
-      });
-    const [isSuccess, setIsSuccess] = useState(false);
+  // add "errors" and "watch" prop to display errors
+  const { register, handleSubmit, formState } = useForm({
+    mode: "onChange",
+  });
+  const [isSuccess, setIsSuccess] = useState(false);
 
-    const onSubmit = data => {
-        console.log(data.jsonFile[0]);
-        const objectURL = URL.createObjectURL(data.jsonFile[0]);
+  const onSubmit = (data) => {
+    console.log(data.jsonFile[0]);
+    const objectURL = URL.createObjectURL(data.jsonFile[0]);
 
-        // fetch(objectURL)
-        //     .then(resp => resp.json())
-        //     .then(console.log);
-
-
-
-        async function getData(url) {
-            const response = await fetch(url);
-            return response.json()
-        }
-        
-        async function runConn() {
-            const jsonData = await getData(objectURL);
-            console.log(jsonData)
-            
-            httpReq(
-                baseUrl + "papers",
-                "POST",
-                JSON.stringify(jsonData)
-            ).then((result) => {
-                setIsSuccess(true);
-                props.onClearGraph(true);
-                props.onSimpleExpand();
-            }).catch (error => {
-                setIsSuccess(false);
-            });
-        }
-
-        runConn();
-        
+    async function getData(url) {
+      const response = await fetch(url);
+      return response.json();
     }
 
+    async function runConn() {
+      const jsonData = await getData(objectURL);
+      console.log(jsonData);
 
-    const onFileChange = data => {
-        setIsSuccess(false);
+      httpReq(baseUrl + "papers", "POST", JSON.stringify(jsonData))
+        .then((result) => {
+          setIsSuccess(true);
+          props.onClearGraph(true);
+          props.onSimpleExpand();
+        })
+        .catch((error) => {
+          setIsSuccess(false);
+        });
     }
 
-    return (
+    runConn();
+  };
 
-        <Form onSubmit={handleSubmit(onSubmit)}>
-        
-            <Form.Group controlId="optionsFormJSON">
-                {/* <Form.Label>Title</Form.Label> */}
-                <Form.File id="jsonFile" placeholder="jsonFile" onChange={onFileChange} name="jsonFile" ref={register()} />
-            </Form.Group>
+  const onFileChange = (data) => {
+    setIsSuccess(false);
+  };
 
-            <div>
-            <Button  variant="primary" type="submit" id="searchButton" name="Search" disabled="true" disabled={!formState.isValid}>
-                Add
-            </Button>
-            {isSuccess &&
-                <h4 style={{color:"green"}}>Success!</h4>
-            }</div>
-        </Form>
-    )
-}
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group controlId="optionsFormJSON">
+        <Form.File
+          id="jsonFile"
+          placeholder="jsonFile"
+          onChange={onFileChange}
+          name="jsonFile"
+          ref={register()}
+        />
+      </Form.Group>
+
+      <div>
+        <Button
+          variant="primary"
+          type="submit"
+          id="searchButton"
+          name="Search"
+          disabled={!formState.isValid}
+        >
+          Add
+        </Button>
+        {isSuccess && <h4 style={{ color: "green" }}>Success!</h4>}
+      </div>
+    </Form>
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {}
-}
+  return {};
+};
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        onClearGraph: (bool) => dispatch(actionCreators.clearNodes(bool)),
-        onSimpleExpand: (sourceNode) => dispatch(actionCreators.simpleExpand(sourceNode))
-    }
-}
+  return {
+    onClearGraph: (bool) => dispatch(actionCreators.clearNodes(bool)),
+    onSimpleExpand: (sourceNode) =>
+      dispatch(actionCreators.simpleExpand(sourceNode)),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(OptionsForm)
+export default connect(mapStateToProps, mapDispatchToProps)(OptionsForm);
--- src/components/OptionsMenu/OptionsMenu.js
+++ src/components/OptionsMenu/OptionsMenu.js
@@ -1,63 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import { slide as Menu } from 'react-burger-menu'
-import OptionsForm from './OptionsForm/OptionsForm'
+import { slide as Menu } from "react-burger-menu";
+import OptionsForm from "./OptionsForm/OptionsForm";
 
 var optionsMenuStyles = {
-    bmBurgerButton: {
-      position: 'fixed',
-      width: '36px',
-      height: '30px',
-      left: '36px',
-      top: '36px',
-      display: 'none'
-    },
-    bmBurgerBars: {
-      background: '#373a47'
-    },
-    bmBurgerBarsHover: {
-      background: '#a90000'
-    },
-    bmCrossButton: {
-      height: '24px',
-      width: '24px'
-    },
-    bmCross: {
-      background: '#bdc3c7'
-    },
-    bmMenuWrap: {
-      position: 'fixed',
-      height: '100%'
-    },
-    bmMenu: {
-      background: '#ebedf0',
-      padding: '2.5em 1.5em 0',
-      fontSize: '1.15em'
-    },
-    bmMorphShape: {
-      fill: '#373a47'
-    },
-    bmItemList: {
-      color: '#b8b7ad',
-      padding: '0.8em'
-    },
-    bmItem: {
-      display: 'inline-block'
-    },
-    bmOverlay: {
-      background: 'rgba(0, 0, 0, 0.3)'
-    }
-  }
-  
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    left: "36px",
+    top: "36px",
+    display: "none",
+  },
+  bmBurgerBars: {
+    background: "#373a47",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    height: "100%",
+  },
+  bmMenu: {
+    background: "#ebedf0",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmMorphShape: {
+    fill: "#373a47",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "inline-block",
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)",
+  },
+};
 
 const optionsMenu = (props) => (
-    <Menu onClose={() => props.optionsClosed()} isOpen={props.options} styles={optionsMenuStyles} right>
-      <div></div> 
-      <h3 className="menuHeader">Options</h3>
-      <p style={{color:"black"}}>Add Nodes With JSON File</p>
-      <OptionsForm />
+  <Menu
+    onClose={() => props.optionsClosed()}
+    isOpen={props.options}
+    styles={optionsMenuStyles}
+    right
+  >
+    <div></div>
+    <h3 className="menuHeader">Options</h3>
+    <p style={{ color: "black" }}>Add Nodes With JSON File</p>
+    <OptionsForm />
   </Menu>
 );
-
 
 export default optionsMenu;
