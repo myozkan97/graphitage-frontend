bb0f38bb4145d54fe6f1eb7573bcce146bb6c824
PartyPancakess <bilgehalilonayci@gmail.com>
2020-07-26 17:52:18 +0300

    changes on options menu

---
+++ src/containers/OptionsMenu/OptionsForm/OptionsManualForm.js
@@ -0,0 +1,88 @@
+import React, { useState, useRef } from "react";
+
+import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
+
+import { connect } from "react-redux";
+
+import * as actionCreators from "../../../store/actions/index";
+import httpReq from "../../../store/actions/utils/http";
+
+
+const OptionsManualForm = (props) => {
+  // const { register, handleSubmit, errors, watch, formState }
+  const { register, handleSubmit } = useForm({
+    mode: "onChange",
+  });
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isCorrectFileType, setIsCorrectFileType] = useState(true);
+  const form = useRef(null);
+
+  const onSubmit = (data) => {
+    const objectURL = URL.createObjectURL(data.jsonFile[0]);
+    let fullName = data.jsonFile[0].name;
+    let fileExtension = fullName.slice((fullName.lastIndexOf('.') + 1));
+
+    async function getData(url) {
+      const response = await fetch(url);
+      return response.json();
+    }
+
+    async function runConn() {
+      console.log("loading start");
+      props.onOpenLoadingScreen();
+
+      const jsonData = await getData(objectURL);
+
+      httpReq("papers", "POST", JSON.stringify(jsonData))
+        .then((result) => {
+          setIsSuccess(true);
+          props.onClearGraph(true);
+          props.onSimpleExpand();
+          
+          console.log("loading finish");
+          props.onCloseLoadingScreen();
+        })
+        .catch((error) => {
+          setIsSuccess(false);
+
+          console.log("loading finish");
+          props.onCloseLoadingScreen();
+        });
+    }
+
+    if(fileExtension.toLowerCase() === "json"){
+      runConn();
+    }else{ //file type not supported
+      // props.onOpenErrorModal("File Type Is Not Supported. Please Choose a \"JSON\" File.");
+      setIsCorrectFileType(false);
+    }
+  };
+
+  const onFileChange = (data) => {
+    setIsSuccess(false);
+    setIsCorrectFileType(true);
+    form.current.dispatchEvent(new Event("submit", { cancelable: true }));
+  };
+
+  return (
+    <p>Form Here</p>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onClearGraph: (bool) => dispatch(actionCreators.clearNodes(bool)),
+    onSimpleExpand: (sourceNode) =>
+      dispatch(actionCreators.simpleExpand(sourceNode)),
+    onOpenErrorModal: (errorMessage) => dispatch(actionCreators.openErrorModal(errorMessage)),
+    onOpenLoadingScreen: () => dispatch(actionCreators.openLoadingScreen()),
+    onCloseLoadingScreen: () => dispatch(actionCreators.closeLoadingScreen()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OptionsManualForm);
--- src/containers/OptionsMenu/OptionsMenu.js
+++ src/containers/OptionsMenu/OptionsMenu.js
@@ -2,6 +2,13 @@ import React from "react";
 
 import { slide as Menu } from "react-burger-menu";
 import OptionsForm from "./OptionsForm/OptionsForm";
+import OptionsManualForm from "./OptionsForm/OptionsManualForm";
+
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortDown } from '@fortawesome/free-solid-svg-icons'
 
 var optionsMenuStyles = {
   bmBurgerButton: {
@@ -28,7 +35,8 @@ var optionsMenuStyles = {
   bmMenuWrap: {
     position: "fixed",
     height: "100%",
-    zIndex: "1000"
+    zIndex: "1000",
+    width: "40%"
   },
   bmMenu: {
     background: "#ebedf0",
@@ -57,10 +65,39 @@ const optionsMenu = (props) => (
     styles={optionsMenuStyles}
     right
   >
-    <div></div>
     <h3 className="menuHeader">Options</h3>
-    <p style={{ color: "black" }}>Add Nodes With JSON File</p>
-    <OptionsForm />
+    <br/><br/>
+    <Card className="text-center">
+      <Card.Header style={{ color: "black" }}>Add Nodes</Card.Header>
+      <Card.Body>
+        <Card.Title style={{ color: "black" }}>With JSON File</Card.Title>
+        <Card.Text >
+          Add a node to the database by selecting a JSON file from local files.
+        </Card.Text>
+        <div id="jaskdjkdas" style={{ transform: "translate(-50%, 0)", left: "50%", width:'60%', position: "relative"}}>
+          <OptionsForm />
+        </div>
+        <hr/>
+        <Card.Title style={{ color: "black" }}>Manually</Card.Title>
+        <Card.Text >
+          Add a node to the database by entering necessary values by hand.
+        </Card.Text>
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Accordion.Toggle as={Card.Header} eventKey="1" style={{ cursor: "pointer" }}>
+              <FontAwesomeIcon icon={faSortDown} />
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>
+                  <OptionsManualForm />
+                </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+    </Card.Body>
+</Card>
+
+      
   </Menu>
 );
 
